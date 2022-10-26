87eaf19b4cc1c7978d1299a9b982315780666a94
Andres-Martinez-Gutierrez <95919805+Andres-Martinez-Gutierrez@users.noreply.github.com>
2022-10-25 22:19:47 -0500

    Se implementa el cambio de opcion del menu

--- lector_qr/lib/providers/ui_provider.dart
+++ lector_qr/lib/providers/ui_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 class UIProvider extends ChangeNotifier {
-  int _selectedMenuOpt = 1;
+  int _selectedMenuOpt = 0;
 
   int get selectedMenuOpt {
     return _selectedMenuOpt;
