e5413443bed9c1c4b7ac8973a5cf52821e61cb9a
Andres-Martinez-Gutierrez <95919805+Andres-Martinez-Gutierrez@users.noreply.github.com>
2022-11-28 08:05:31 -0500

    Optimizando las pantallas direcciones y mapas

--- lector_qr/lib/pages/direcciones_page.dart
+++ lector_qr/lib/pages/direcciones_page.dart
@@ -1,44 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'package:lector_qr/providers/scan_list_provider.dart';
+import 'package:lector_qr/widgets/scan_list.dart';
 
 class DireccionesPage extends StatelessWidget {
   const DireccionesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context);
-    final scans = scanListProvider.scans;
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, index) => Dismissible(
-        key: Key(scans[index].id.toString()),
-        //key: UniqueKey(),
-        background: Container(
-          color: Colors.red,
-        ),
-        onDismissed: (DismissDirection direction) {
-          Provider.of<ScanListProvider>(context, listen: false)
-              .borrarScanPorId(scans[index].id!);
-        },
-
-        child: ListTile(
-          leading: Icon(
-            Icons.assistant_direction,
-            color: Theme.of(context).primaryColor,
-          ),
-          title: Text(
-            scanListProvider.scans[index].valor,
-          ),
-          subtitle: Text('${scans[index].id}'),
-          trailing: const Icon(
-            Icons.keyboard_arrow_right,
-            color: Colors.grey,
-          ),
-          onTap: (() => print(scans[index].id.toString())),
-        ),
-      ),
-    );
+    return const ScanList(tipo: 'http');
   }
 }
