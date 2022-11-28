146a7e523566dafffdab6927b4cba19a85ab4762
Andres-Martinez-Gutierrez <95919805+Andres-Martinez-Gutierrez@users.noreply.github.com>
2022-11-28 12:05:18 -0500

    Se agrega libreria de google maps y se integra al proyecto

--- lector_qr/lib/pages/mapa_page.dart
+++ lector_qr/lib/pages/mapa_page.dart
@@ -1,19 +1,39 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import 'package:lector_qr/models/scan_model.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
   const MapaPage({super.key});
 
   @override
+  State<MapaPage> createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  @override
   Widget build(BuildContext context) {
+    final CameraPosition puntoInicial = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
+    Completer<GoogleMapController> _controller = Completer();
     final ScanModel scan =
         ModalRoute.of(context)!.settings.arguments as ScanModel;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Coordenadas'),
       ),
-      body: Center(
-        child: Text(scan.valor),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
