03bce44ee959ce338e59177776949d0545276f7f
Andres-Martinez-Gutierrez <95919805+Andres-Martinez-Gutierrez@users.noreply.github.com>
2022-11-30 15:47:56 -0500

    Agregando marcador de posicion al la localizacion

--- lector_qr/lib/pages/mapa_page.dart
+++ lector_qr/lib/pages/mapa_page.dart
@@ -14,22 +14,31 @@ class MapaPage extends StatefulWidget {
 }
 
 class _MapaPageState extends State<MapaPage> {
+  //
+  final Completer<GoogleMapController> _controller = Completer();
+
+  //
   @override
   Widget build(BuildContext context) {
-    final CameraPosition puntoInicial = CameraPosition(
-        target: LatLng(37.42796133580664, -122.085749655962),
-        zoom: 17.5,
-        tilt: 50);
-
-    Completer<GoogleMapController> _controller = Completer();
     final ScanModel scan =
         ModalRoute.of(context)!.settings.arguments as ScanModel;
+
+    final CameraPosition puntoInicial =
+        CameraPosition(target: scan.getLatLng(), zoom: 17.5, tilt: 50);
+
+    // ignore: prefer_collection_literals
+    final Set<Marker> markers = Set<Marker>();
+    markers.add(Marker(
+      markerId: const MarkerId('geo-location'),
+      position: scan.getLatLng(),
+    ));
     return Scaffold(
       appBar: AppBar(
         title: const Text('Coordenadas'),
       ),
       body: GoogleMap(
-        myLocationButtonEnabled: truegit,
+        markers: markers,
+        myLocationButtonEnabled: true,
         mapType: MapType.normal,
         initialCameraPosition: puntoInicial,
         onMapCreated: (GoogleMapController controller) {
